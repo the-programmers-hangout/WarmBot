48705c33795390e3d869861d5380aef44a848947
Elliott Short <elliott.short@gmail.com>
2019-03-14 23:03:39 -0700

    Initial draft of thunbnails in generated embeds.

--- src/main/kotlin/me/aberrantfox/warmbot/MainApp.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/MainApp.kt
@@ -1,22 +1,29 @@
 package me.aberrantfox.warmbot
 
 import me.aberrantfox.kjdautils.api.startBot
+import me.aberrantfox.warmbot.extensions.htmlString
 import net.dv8tion.jda.core.entities.Game
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     val token = args.first()
 
-    if(token == "UNSET") {
+        if(token == "UNSET") {
         println("You must specify the token with the -e flag when running via docker.")
         System.exit(-1)
     }
 
     startBot(token) {
+
         configure {
             prefix = "!"
             globalPath = "me.aberrantfox.warmbot"
         }
 
+
         jda.presence.setPresence(Game.of(Game.GameType.DEFAULT, "DM to contact Staff"), true)
+//
+        jda.getTextChannelById("555984239343042573").htmlString()
+         exitProcess(0)
     }
 }
--- src/main/kotlin/me/aberrantfox/warmbot/extensions/ChannelExtension.kt
+++ src/main/kotlin/me/aberrantfox/warmbot/extensions/ChannelExtension.kt
@@ -33,11 +33,10 @@ fun MessageChannel.htmlString(): File {
 
                     if (it.embeds.isNotEmpty() && it.author.isBot) {
                         embedToHTML(it.embeds.first(), this)
-                    }
-                    else {
+                    } else {
                         p {
                             style = "color: #D7DAD4"
-                            + it.fullContent()
+                            +it.fullContent()
                         }
                     }
                 }
@@ -51,41 +50,66 @@ fun MessageChannel.htmlString(): File {
 private fun embedToHTML(messageEmbed: MessageEmbed, div: DIV) = div.apply {
     div {
         style =
-            "width: 500px;" +
-            "border-radius: 5px;" +
-            "border-left-style: solid;" +
-            "border-left-width: 5px;" +
-            "border-left-color: ${messageEmbed.color.toHex()};" +
-            "background-color: #34363C;"
+                "width: 100%;" +
+                        "max-width: 500px;" +
+                        "min-width: 300px;" +
+                        "height: auto;" +
+                        "overflow: visible;" +
+                        "display: flex;" +
+                        "flex-direction: column;" +
+                        "border-radius: 5px;" +
+                        "border-left-style: solid;" +
+                        "border-left-width: 5px;" +
+                        "border-left-color: ${messageEmbed.color.toHex()};" +
+                        "background-color: #34363C;"
 
         div {
             style =
-                "padding-left: 10px;" +
-                "padding-top: 15px;" +
-                "line-height: 10px;"
+                    "padding-left: 10px;" +
+                            "padding-top: 15px;" +
+                            "line-height: 10px;"
 
             if (messageEmbed.title != null) {
                 div {
-                    style ="color: #F1F1F1;font-size: 15px;"
-                    + messageEmbed.title
+                    style = "color: #F1F1F1;font-size: 15px;"
+                    +messageEmbed.title
                 }
             }
 
             if (messageEmbed.description != null)
                 div {
                     style = "color: #AAA9AD;font-size: 14px;Line-Height: 20px"
-                    + messageEmbed.description
+                    +messageEmbed.description
                 }
+
+
+            if (messageEmbed.thumbnail.url != null) {
+                img {
+                    style = "height: 80px;" +
+                            "width: 80px;" +
+                            "position: relative;" +
+                            "float: right;" +
+                            "bottom: 8px;" +
+                            "margin-right: 7px;" +
+                            "border-radius: 5px;" +
+                            "border-style: solid;" +
+                            "border-color: transparent;"
+
+                    src = messageEmbed.thumbnail.url
+                }
+
+            }
+
 
             messageEmbed.fields.forEach {
                 div {
-                    style ="color: #F1F1F1;font-size: 15px;padding-bottom: 10px;"
-                    + it.name
+                    style = "color: #F1F1F1;font-size: 15px;padding-bottom: 10px;"
+                    +it.name
                 }
 
                 div {
                     style = "color: #AAA9AD;font-size: 14px;padding-bottom: 10px;Line-Height: 20px"
-                    + it.value
+                    +it.value
                 }
             }
         }
@@ -93,11 +117,11 @@ private fun embedToHTML(messageEmbed: MessageEmbed, div: DIV) = div.apply {
 }
 
 private fun intoStream(doc: Element, out: OutputStream) =
-    with(TransformerFactory.newInstance().newTransformer()){
-        setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no")
-        setOutputProperty(OutputKeys.METHOD, "xml")
-        setOutputProperty(OutputKeys.INDENT, "yes")
-        setOutputProperty(OutputKeys.ENCODING, "UTF-8")
-        setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4")
-        transform(DOMSource(doc), StreamResult(OutputStreamWriter(out, "UTF-8")))
-    }
+        with(TransformerFactory.newInstance().newTransformer()) {
+            setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no")
+            setOutputProperty(OutputKeys.METHOD, "xml")
+            setOutputProperty(OutputKeys.INDENT, "yes")
+            setOutputProperty(OutputKeys.ENCODING, "UTF-8")
+            setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4")
+            transform(DOMSource(doc), StreamResult(OutputStreamWriter(out, "UTF-8")))
+        }
